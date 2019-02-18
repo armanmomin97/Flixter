97d2375bd902adfc4423f8238df1beb1654efa80
Arman Momin <Arman1@wireless-10-146-41-118.public.utexas.edu>
2019-02-18 17:54:45 -0600

    part 2

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Arman Momin on 2/18/19.
+//  Copyright © 2019 armanmomin. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
